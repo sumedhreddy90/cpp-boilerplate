f7dc912cf7c09c071551b3d3dbb0d3970242eea7
Sumedh Reddy <sumedhreddykoppula@gmail.com>
2021-10-02 16:20:22 -0400

    Adding cpplint and cppcheck files to result folder

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,9 @@
+//! "Copyright [2021] Sumedh Koppula, Pratik Bhujbal"
+/**
+* @file main.cpp
+* @author Sumedh Koppula, Pratik Bhujbal
+* @brief This is a the main method for the PID implementation project
+*/
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
